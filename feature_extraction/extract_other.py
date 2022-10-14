6fbd853c2582ab6446d0ca35f68cec7f3c5f98ef
kheitm <kathyann.heitmeier@gmail.com>
2022-10-14 09:15:37 +0200

    init other feats

---
+++ feature_extraction/extract_other.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import librosa
+import opensmile
+
+def extract_egemaps(wav_file):
+    audio, sr = librosa.load(wav_file) 
+    smile = opensmile.Smile(feature_set=opensmile.FeatureSet.eGeMAPSv02,
+        num_channels=1, feature_level=opensmile.FeatureLevel.LowLevelDescriptors,)
+    row_data = smile.process_signal(audio, sr) # <class 'pandas.core.frame.DataFrame'
+    return row_data
+
+def egemaps_dataset(fname):
+    features = [] 
+    df = pd.read_csv(fname, sep='\t')
+    for _, row in df.iterrows():
+        wav_file, mmse, label = row[0], row[4], row[5]
+        try:
+            data = extract_egemaps(wav_file)
+            funcs_as_list = data.values.tolist()
+            features.append([label, mmse, funcs_as_list])
+        except (IOError, ValueError):
+             print("An I/O error or a ValueError occurred with file {}".format(wav_file))
+
+    dataset = pd.DataFrame(features, columns=['label', 'mmse', 'functionals']) 
+    dataset.to_json('/Users/kathy-ann/thesis/lld_feats.json', orient="values")
+
+    return dataset
+
+fname = '/Users/kathy-ann/thesis/data.tsv'
+egemaps_dataset(fname)
