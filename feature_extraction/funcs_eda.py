e1d3db0daa4f942ee584ae1ce3066ddf46e33a8c
Kathy-Ann Heitmeier <61460502+kheitm@users.noreply.github.com>
2023-06-26 17:59:28 +0200

    Update funcs_eda.py

--- feature_extraction/funcs_eda.py
+++ feature_extraction/funcs_eda.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 #%%
 # load funcs and meta data to one df
-meta = '/Users/kathy-ann/thesis/funcs_meta1.csv'
-funcs = '/Users/kathy-ann/thesis/final_funcs1.csv'
+meta = '.../funcs_meta1.csv'
+funcs = '.../final_funcs1.csv'
 meta = pd.read_csv(meta, index_col=0)
 funcs = pd.read_csv(funcs).drop(['start', 'end'], axis=1)
 df = pd.concat([meta, funcs], axis = 1)
-df
-# %%
+
+
